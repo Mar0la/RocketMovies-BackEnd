68bc612f3bd672ac40b27780418b60b8ef73e065
Matheus Marins <matheus.marins2011@hotmail.com>
2023-10-30 09:57:35 -0300

    add files for deploy

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+require("dotenv/config")
 require("express-async-errors")
 
 const cors = require('cors')
@@ -28,5 +29,5 @@ app.use((error, request, response, next)  => {
   })
 })
 
-const PORT = 3333
+const PORT = process.env.PORT || 3333
 app.listen(PORT, () => console.log(`Server rodando na porta ${PORT}`))
